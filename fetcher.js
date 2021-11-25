28176422f6a127fc77f73ed64d2beca1b79bfac5
Mohammed Abu-Zeinah <mabuzainah@gmail.com>
2021-11-25 12:36:16 -0500

    Update fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -5,22 +5,23 @@ const fs = require("fs");
 const commandLineArgument = process.argv.slice(2);
 const urlPassed = commandLineArgument[0];
 const path = commandLineArgument[1];
-
+let content = "";
 const fetcher = function (urlPassed, path) {
   request(urlPassed, function (error, response, body) {
-  console.error('error:', error); // Print the error if one occurred
-  console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-  console.log('body:', body); // Print the HTML for the Google homepage.
-  });
-  //write to a local file path passed to the fetcher function. 
-  fs.writeFile(path, body, (err) => {
-    if (err)
-      console.log(err);
-    else {
-      console.log("File written successfully\n");
-      console.log("The written has the following contents:");
-      console.log(fs.readFileSync("books.txt", "utf8"));
-    }
+    console.error('error:', error); // Print the error if one occurred
+    console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
+    content = body;
+    //console.log('body:', content); // Print the HTML for the Google homepage.
+    //write to a local file path passed to the fetcher function, if error is present throw the 
+    // error otherwise indicate succcess
+    fs.writeFile(path, content, (err) => {
+      if (err) {
+        console.log("An error occurred" + err);
+      } else {
+        console.log("File written successfully\n");
+        console.log("Downloaded and saved " + fs.statSync(path).size + " bytes to " + path);
+      }
+    });
   });
 };
 
